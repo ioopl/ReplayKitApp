a2415b8e99a9d2a12f27891d2ecea8429ed01687
Umair Hasan <ioopl.com@gmail.com>
2026-07-26 12:45:15 +0100

    Initial Commit

---
+++ ReplayKitApp/ReplayKitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReplayKitAppApp.swift
+//  ReplayKitApp
+//
+//  Created by Umair Hasan on 26/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct ReplayKitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
