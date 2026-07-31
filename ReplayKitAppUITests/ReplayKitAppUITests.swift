edfc3c0dd0f4a4ca9139ff36a62738a109ceb59b
Umair Hasan <ioopl.com@gmail.com>
2026-07-31 22:15:47 +0100

    Chore: Recorded UI Tests

--- ReplayKitAppUITests/ReplayKitAppUITests.swift
+++ ReplayKitAppUITests/ReplayKitAppUITests.swift
@@ -1,43 +1,39 @@
-//
-//  ReplayKitAppUITests.swift
-//  ReplayKitAppUITests
-//
-//  Created by Umair Hasan on 26/07/2026.
-//
-
 import XCTest
 
 final class ReplayKitAppUITests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
     @MainActor
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    func testWelcomeScreenAndMenuNavigation() throws {
         let app = XCUIApplication()
         app.launch()
 
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // XCUIAutomation Documentation
-        // https://developer.apple.com/documentation/xcuiautomation
-    }
-
-    @MainActor
-    func testLaunchPerformance() throws {
-        // This measures how long it takes to launch your application.
-        measure(metrics: [XCTApplicationLaunchMetric()]) {
-            XCUIApplication().launch()
-        }
+        // Verify title exists
+        let welcomeHeader = app.staticTexts["Broadcast Client"]
+        XCTAssertTrue(welcomeHeader.exists)
+        
+        // Verify all 4 options are visible as button/cards
+        XCTAssertTrue(app.staticTexts["A) In-App Screen Recording"].exists)
+        XCTAssertTrue(app.staticTexts["B) In-App Raw Frame Capture"].exists)
+        XCTAssertTrue(app.staticTexts["C) System Wide - Screen Broadcast"].exists)
+        XCTAssertTrue(app.staticTexts["D) Rolling Clips Recording"].exists)
+        
+        // Tap first option card to navigate to In-App Screen Recording view
+        app.staticTexts["A) In-App Screen Recording"].tap()
+        
+        // Verify we navigated to the recording screen
+        XCTAssertTrue(app.staticTexts["In-App Screen Recording"].exists)
+        
+        // Go back
+        app.navigationBars.buttons.element(boundBy: 0).tap()
+        
+        // Verify we are back on the welcome dashboard
+        XCTAssertTrue(app.staticTexts["Broadcast Client"].exists)
     }
 }
